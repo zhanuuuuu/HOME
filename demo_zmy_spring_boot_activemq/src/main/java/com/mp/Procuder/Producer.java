d7ee53da7f5df057d15a1fecbfd02ad7fcb62e78
zhang <1084716544@qq.com>
2018-04-02 10:56:45 +0800

    加了定时器  我们在receiveQueue方法上面多加了一个注解@SendTo("out.queue")，该注解的意思是将return回的值，再发送的"out.queue"队列中，下面我们再来跑一下前面的测试，在监控页面中，我们发现，"out.queue"队列中已经有内容了，如下

--- demo_zmy_spring_boot_activemq/src/main/java/com/mp/Procuder/Producer.java
+++ demo_zmy_spring_boot_activemq/src/main/java/com/mp/Procuder/Producer.java
@@ -3,6 +3,7 @@ package com.mp.Procuder;
 import javax.jms.Destination;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,4 +15,9 @@ public class Producer {
     public void sendMessage(Destination destination, final String message){
         jmsTemplate.convertAndSend(destination, message);
     }
+
+//    @JmsListener(destination="out.queue")
+//    public void consumerMessage(String text){
+//        System.out.println("从out.queue队列收到的回复报文为:"+text);
+//    }
 }
